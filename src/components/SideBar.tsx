4204aaba16602ff486de206872bf4285d8aaffc0
Raphael0025 <rpbisla0025@gmail.com>
2024-04-12 05:15:04 +0800

    sidebar ok

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -22,11 +22,11 @@ export default function SideBar(){
             </div>
             <div className='w-full flex flex-col h-full justify-between'>
                 <div className='flex flex-col justify-between'>
-                    <Link href='/admin/dashboard' className='py-4 px-3 text-center font-medium text-xs hover:bg-red-700'>Dashboard</Link>
+                    <Link href='/admin/dashboard' className='py-4 px-3 text-center font-medium text-xs hover:bg-red-700'>Analytics</Link>
                     <Link href='/admin/publications' className='py-4 px-3 text-center font-medium text-xs hover:bg-red-700'>Publications</Link>
-                    <Link href='/admin/student-requests' className='py-4 px-3 text-center font-medium text-xs hover:bg-red-700'>Student Inquiry</Link>
-                    <Link href='/admin/content-management' className='py-4 px-3 text-center font-medium text-xs hover:bg-red-700'>Posts</Link>
-                    <Link href='/admin/student-management' className='py-4 px-3 text-center font-medium text-xs hover:bg-red-700'>Students</Link>
+                    <Link href='/admin/student-requests' className='py-4 px-3 text-center font-medium text-xs hover:bg-red-700'>Student Requests</Link>
+                    <Link href='/admin/content-management' className='py-4 px-3 text-center font-medium text-xs hover:bg-red-700'>Announcements</Link>
+                    <Link href='/admin/student-management' className='py-4 px-3 text-center font-medium text-xs hover:bg-red-700'>Registered Users</Link>
                 </div>
                 
                 <button onClick={handleLogout} className='py-4 px-3 text-center font-medium text-xs hover:bg-red-700'>Logout</button>
